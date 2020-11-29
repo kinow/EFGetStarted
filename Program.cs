1ab4a762b72d9a93a5f9a09987a805ba6958adb7
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-11-30 08:54:43 +1300

    Use DbContext to interact with database (crud)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using EFGetStarted.Models;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 
 namespace EFGetStarted
 {
@@ -6,7 +9,38 @@ namespace EFGetStarted
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new Model.BloggingContext())
+            {
+                // Create
+                Console.WriteLine("Inserting a new blog");
+                db.Add(new Blog {Url = "http://google.com/"});
+                db.SaveChanges();
+                
+                // Read
+                Console.WriteLine("Querying for a blog");
+                var blog = db.Blogs
+                    .OrderBy(b => b.BlogId)
+                    .First();
+                
+                // Update
+                Console.WriteLine("Updating the blog and adding a post");
+                blog.Url = "https://duckduckgo.com/";
+                blog.Posts.Add(
+                    new Post
+                    {
+                        Title = "Hello World",
+                        Content = "I wrote an app using EF Core!"
+                    }
+                );
+                db.SaveChanges();
+                
+                // Delete
+                Console.WriteLine("Delete the blog");
+                db.Remove(blog);
+                db.SaveChanges();
+                
+                Console.WriteLine("Bye!");
+            }
         }
     }
 }
